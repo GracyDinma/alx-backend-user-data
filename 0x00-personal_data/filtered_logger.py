75b952e0a56624c7084e5839d0c347791a057851
GracyDinma <okohagchidinma@gmail.com>
2024-11-09 20:20:29 +0000

    filtered log up

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,14 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """Returns regex obfuscated log messages."""
+    """
+     Args:
+        fields: list of fields to redact
+        redaction: the value to use for redaction
+        message: the string message to filter
+        separator: the separator to use between fields
+
+    Returns regex obfuscated log messages.
+    """
     return re.sub(rf"({'|'.join(fields)})=[^{separator}]*",
                   lambda m: f"{m.group(1)}={redaction}", message)
