09c07316b2bc461dd27473482c26138bae3f6805
GracyDinma <okohagchidinma@gmail.com>
2024-11-09 20:45:24 +0000

    task 1 added

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,25 @@
 """Use of regex in replacing occurences of certain field values."""
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Returns filtered values from log records."""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
