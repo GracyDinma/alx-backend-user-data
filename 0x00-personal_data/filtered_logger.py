7da039378a5f39662cf24dff799fb9c6a41b3c43
GracyDinma <okohagchidinma@gmail.com>
2024-11-09 20:36:34 +0000

    updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3i
+"""Use of regex in replacing occurences of certain field values."""
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """ Returns regex obfuscated log messages """
-    for field in fields:
-        message = re.sub(f'{field}=(.*?){separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
+    """Returns regex obfuscated log messages."""
+    return re.sub(rf"({'|'.join(fields)})=[^{separator}]*",
+                  lambda m: f"{m.group(1)}={redaction}", message)
