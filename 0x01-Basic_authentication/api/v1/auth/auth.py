bb8c5065dc860573d6be600efbe93789bf5bcd22
GracyDinma <okohagchidinma@gmail.com>
2024-11-12 09:12:23 +0000

    updated

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
-""" Auth class for API authentication"""
+"""
+Auth class for API authentication.
+"""
+
+
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
+    """ _summary_
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Check if authentication is required for the given path.
+
         Args:
             path: The requested URL path.
             excluded_paths: List of paths where authentication is not required
@@ -19,7 +27,8 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """
-        Get the authorization header.
+        Get the authorization header
+
         Args:
             request: The Flask request object.
 
@@ -31,10 +40,12 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """
         Display current user from the request.
+
         Args:
             request: The Flask request object.
 
         Returns:
             None
         """
+
         return None
