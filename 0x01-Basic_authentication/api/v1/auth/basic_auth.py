c66e34b5196f906375414572a01d6f1e9e0e5df5
GracyDinma <okohagchidinma@gmail.com>
2024-11-15 11:49:45 +0000

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,8 @@ This module defines the BasicAuth class.
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -83,3 +85,33 @@ class BasicAuth(Auth):
         user_email, user_password = decoded_base64_authorization_header.split(
                                     ':', 1)
         return user_email, user_password
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """
+        A method that reurns the user instance based on his email
+        and password.
+
+        Args:
+            user_email(str): The user's email address.
+            user_pwd(str): The user's password.
+
+        Returns:
+            User: The User instance if credentials are valid.
+        """
+
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        """ Search for the user in the databse by email."""
+        users = User.search({"email": user_email})
+        if not users or len(users) == 0:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
