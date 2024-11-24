4d4070402617ce53ed017a49c7ff20c9384dd1e8
GracyDinma <okohagchidinma@gmail.com>
2024-11-24 04:26:19 +0000

    updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,16 +3,54 @@
 Define _hash_password method.
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from bcrypt import hashpw, gensalt
 
 
-def _hash_password(password: str) -> bytes:
+class Auth:
     """
-    Hash a password using bcrypt.
-
-    Args:
-        password (str): The plain text password to hash.
-
-    Returns:
-        bytes: The hashed password as bytes.
+    Auth class to interact with the authentication database.
     """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user.
+
+        Args:
+            email (str): The user's email.
+            password (str): The user's password.
+
+        Returns:
+            User: The newly created user object.
+
+        Raises:
+            valueError: If a user with the given email already exists.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            # if no exception is raised, user exists
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # User does not exist; proceed with registration
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            self._db._session.rollback()
+            return new_user
+
+    @staticmethod
+    def _hash_password(password: str) -> bytes:
+        """
+        Hash a password using bcrypt.
+
+        Args:
+            password (str): The plain text password to hash.
+
+        Returns:
+            bytes: The hashed password as bytes.
+        """
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
